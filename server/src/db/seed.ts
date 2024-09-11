bccb42b3b7804c261e32d50da98cf0f31de11f26
Pedro Germano <pedrogermano232@gmail.com>
2024-09-11 11:46:23 -0300

    first route

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -18,8 +18,8 @@ async function seed() {
   const startOfWeek = dayjs().startOf("week");
 
   await db.insert(goalCompletions).values([
-    { goalId: result[0].id, createdAt: new Date() },
-    { goalId: result[1].id, createdAt: new Date() },
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, "day").toDate() },
   ]);
 }
 
